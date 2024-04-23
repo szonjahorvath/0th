58bb8d9d1d8bf367135188f2ba1d73cdee132a97
Szonja Horvath <horvathszonja@gmail.com>
2024-04-23 15:03:15 +0200

    Initial commit of the empty project

---
+++ ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/ZerothWeekWorkshopApplication.java
@@ -0,0 +1,13 @@
+package org.example.zerothweekworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerothWeekWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerothWeekWorkshopApplication.class, args);
+    }
+
+}
---
+++ ZerothWeekWorkshop/src/test/java/org/example/zerothweekworkshop/ZerothWeekWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zerothweekworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerothWeekWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
