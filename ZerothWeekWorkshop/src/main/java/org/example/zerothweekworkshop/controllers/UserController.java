3e074a9143cd7b7fdf0b10b96030183fa336c01c
Laszlo Szilagyi <szilagyi.laszlo989@gmail.com>
2024-04-28 19:41:49 +0200

    in login page a sign up button added, signup html created

--- ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/controllers/UserController.java
+++ ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/controllers/UserController.java
@@ -1,13 +1,19 @@
 package org.example.zerothweekworkshop.controllers;
 
+import org.example.zerothweekworkshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/singup")
-    public String singup() {
-        return "singup";
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
     }
 }
