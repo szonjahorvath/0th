87c02ae406b5252c0943d28b168d2a3fef14070f
Máté Holló <hollomate1106@gmail.com>
2024-04-28 10:09:15 +0200

    Let throw css on SecurityConfig

--- ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/configurations/SecurityConfig.java
+++ ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/configurations/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(
+                                "/style/**",
                                 "/",
                                 "/login",
                                 "/addNewUser",
