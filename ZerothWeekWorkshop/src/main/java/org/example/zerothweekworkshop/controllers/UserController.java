93ef0d2649792b53c3b83e24abb88cc5ab14a004
Laszlo Szilagyi <szilagyi.laszlo989@gmail.com>
2024-04-28 18:53:50 +0200

    signup endpoint in UserController class added

--- ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/controllers/UserController.java
+++ ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/controllers/UserController.java
@@ -1,4 +1,13 @@
 package org.example.zerothweekworkshop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/singup")
+    public String singup() {
+        return "singup";
+    }
 }
