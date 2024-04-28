9f5033c2ea01071c6d5b7136a0b7f45cf6cd4c92
Wanda Imrich <imrich.wanda@gmail.com>
2024-04-28 11:13:00 +0200

    Login page styled vol1

--- ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/configurations/SecurityConfig.java
+++ ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/configurations/SecurityConfig.java
@@ -1,4 +1,47 @@
 package org.example.zerothweekworkshop.configurations;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests //authorize which area can access to everyone
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()   // free to see everbody
+                )
+                .formLogin((form) -> form       // for everybody
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll()
+                            .logoutSuccessUrl("/login?logout")
+                );       //what happens if you logged out
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
--- ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/controllers/MainController.java
+++ ZerothWeekWorkshop/src/main/java/org/example/zerothweekworkshop/controllers/MainController.java
@@ -1,13 +1,33 @@
 package org.example.zerothweekworkshop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
+
 public class MainController {
 
+    @GetMapping("/")
     public String mainPage() {
         return "index";
     }
+
+    @GetMapping("/content")
+    public String content() {
+        return "content";
+    }
+
+
+    @GetMapping("/login")
+    public String loginForm(@RequestParam(value = "logout", required = false) String logout) {
+        return "login";
+    }
+
+ /*   @GetMapping("/login")
+    public String loginForm(){
+        return "login";
+    }*/
 }
